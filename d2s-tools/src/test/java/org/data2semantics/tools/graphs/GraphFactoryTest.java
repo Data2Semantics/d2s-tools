d50caf222aaa3970fd4e17d5cc57c8bf8e27da51
Gerben <Gerben@145.18.215.190>
2012-06-11 13:56:19 +0200

    Updated GraphFactoryTest

--- d2s-tools/src/test/java/org/data2semantics/tools/graphs/GraphFactoryTest.java
+++ d2s-tools/src/test/java/org/data2semantics/tools/graphs/GraphFactoryTest.java
@@ -19,12 +19,13 @@ public class GraphFactoryTest {
 	@Test
 	public void test() {
 		RDFDataSet testSet = new RDFFileDataSet("D:\\workspaces\\eclipse_workspace\\rdfgraphlearning\\src\\main\\resources\\aifb-fixed_complete.rdf", RDFFormat.RDFXML);
-		org.openrdf.model.Graph triples = testSet.getStatements(null, "http://swrc.ontoware.org/ontology#affiliation", null, true);
-		
+		//org.openrdf.model.Graph triples = testSet.getStatements(null, "http://swrc.ontoware.org/ontology#affiliation", null, true);
+		org.openrdf.model.Graph triples = testSet.getFullGraph();
+			
 		DirectedGraph<String, Edge<String>> jungGraph = GraphFactory.createJUNGGraph(triples);
 		
 		for (Edge<String> edge: jungGraph.getEdges()) {
-			System.out.println(edge);
+			System.out.println(jungGraph.getEndpoints(edge).getFirst() + " " + edge + " " + jungGraph.getEndpoints(edge).getSecond());
 		}
 	}
 }
