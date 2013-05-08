82d5cb65c625ea6140ead7b470b767b87c6baa4d
Gerben <Gerben@145.18.213.108>
2013-05-08 18:23:52 +0200

    Added Binary Vector

--- kernelexperiments/src/main/java/org/data2semantics/exp/FullThemeExperiment.java
+++ kernelexperiments/src/main/java/org/data2semantics/exp/FullThemeExperiment.java
@@ -33,10 +33,10 @@ public class FullThemeExperiment extends CompareExperiment {
 		double[] cs = { 1, 10, 100, 1000};	
 		// 0.001, 0.01, 0.1,
 		int depth = 3;
-		int[] iterations = {0, 2, 4};
+		int[] iterations = {0, 2, 4, 6};
 
 		dataset = new RDFFileDataSet("C:\\Users\\Gerben\\Dropbox\\data_bgs_ac_uk_ALL", RDFFormat.NTRIPLES);
-		createGeoDataSet(10, "http://data.bgs.ac.uk/ref/Lexicon/hasTheme");
+		createGeoDataSet(50, "http://data.bgs.ac.uk/ref/Lexicon/hasTheme");
 
 	
 		ResultsTable resTable = new ResultsTable();
@@ -86,7 +86,7 @@ public class FullThemeExperiment extends CompareExperiment {
 			}
 
 			for (Statement stmt2 : stmts2) {
-				if (Math.random() < 0.01) {
+				if (Math.random() < 0.05) {
 					instances.add(stmt2.getSubject());
 					labels.add(stmt2.getObject());
 				}
