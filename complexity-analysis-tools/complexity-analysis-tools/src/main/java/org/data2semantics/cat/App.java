cd4d25e31b37ed3a319c2f7235fc83a02c65cd97
Peter Bloem <p@peterbloem.nl>
2013-06-10 10:11:44 +0100

    Removed need for init file

--- complexity-analysis-tools/complexity-analysis-tools/src/main/java/org/data2semantics/cat/App.java
+++ complexity-analysis-tools/complexity-analysis-tools/src/main/java/org/data2semantics/cat/App.java
@@ -3,7 +3,11 @@ package org.data2semantics.cat;
 import java.io.File;
 import java.io.IOException;
 
+import org.lilian.experiment.Environment;
+import org.lilian.experiment.Experiment;
+import org.lilian.experiment.Resources;
 import org.lilian.experiment.Run;
+import org.lilian.graphs.DTGraph;
 
 /**
  * Hello world!
@@ -11,10 +15,18 @@ import org.lilian.experiment.Run;
  */
 public class App 
 {
+	private static final File DATA = new File("/Users/Peter/Documents/datasets/graphs/aifb/aifb-fixed_complete.owl");
+	private static final File ENVIRONMENT = new File("/Users/Peter/Experiments/d2s-cat/");
+	
     public static void main( String[] args ) throws IOException
     {
-    	File dir = new File("/Users/Peter/Experiments/graphs/commit/");
+    	DTGraph<String, String> data = Resources.rdfGraph(DATA);
     	
-    	Run.run(dir);
+    	Experiment experiment = new GraphMeasures<String>(data, "large");
+    	
+    	Environment env = new Environment(ENVIRONMENT, 0);
+    	Environment.current = env;
+    	
+    	experiment.run();
     }
 }
