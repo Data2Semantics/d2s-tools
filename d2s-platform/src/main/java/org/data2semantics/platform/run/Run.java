5d62a2219f490b3fa1c57a08bf3018d96b577b96
Peter Bloem <p@peterbloem.nl>
2013-09-12 15:29:04 +0200

    fix no-argument bug

--- d2s-platform/src/main/java/org/data2semantics/platform/run/Run.java
+++ d2s-platform/src/main/java/org/data2semantics/platform/run/Run.java
@@ -65,7 +65,7 @@ public class Run
     		file = new File(run.arguments.get(0));
     	
     	if(! file.exists())
-    		usageExit("Workflow file ("+run.arguments.get(0)+") does not exist.", parser);
+    		usageExit("Workflow file ("+file+") does not exist.", parser);
     	
     	// -- Beyond this point, errors are not the user's fault, and should not
     	//    cause a usage print. 
