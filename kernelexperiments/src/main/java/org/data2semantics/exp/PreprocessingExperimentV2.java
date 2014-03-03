ad230811734da69a345d2c23d22e7fe441fd86b3
Gerben <Gerben@Guinness>
2014-03-03 14:36:23 +0100

    update

--- kernelexperiments/src/main/java/org/data2semantics/exp/PreprocessingExperimentV2.java
+++ kernelexperiments/src/main/java/org/data2semantics/exp/PreprocessingExperimentV2.java
@@ -84,7 +84,7 @@ public class PreprocessingExperimentV2 extends RDFMLExperiment {
 	public static void experiment(boolean fullGraph) {
 
 		long[] seeds = {11,21,31,41,51,61,71,81,91,101};
-		double[] cs = {0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000};	
+		double[] cs = {1, 10, 100, 1000, 10000, 100000, 1000000};	
 
 		// --------------
 		// Learning Algorithm settings
